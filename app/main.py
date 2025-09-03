b52993957efee23e501672a8b39648f4298bda7e
brayohmurithidev <murithibrianm@gmail.com>
2025-09-03 22:23:59 +0300

    updated allowed routes

--- app/main.py
+++ app/main.py
@@ -21,13 +21,14 @@ app = FastAPI(
     },
 )
 
-# origins = [
-#     "http://localhost:3000",
-#     "http://localhost:8000",
-#     "http://localhost:5173",
-#     "https://donatehub-tenant.vercel.app"
-# ]
-origins = ["*"]
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:5173",
+    "https://donatehub-tenant.vercel.app"
+    "https://staging.donatehub-tenant.fazilabs.com",
+    "https://donatehub-tenant.fazilabs.com"
+]
 
 app.add_middleware(
     CORSMiddleware,
